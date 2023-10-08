afe5f6b5b5115e1b5271972e1f5cd06332b1523b
Kamallochan Das <118735381+kamallochan2004@users.noreply.github.com>
2023-10-08 23:15:01 +0530

    Revert "FIXED: Error's that user is getting when someone tries to run your pr…"

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ inquirer
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
-      console.log("Prompt couldn't be rendered in the current environment");
     } else {
       // Something else went wrong
-      console.log(error)
     }
   });
 /* 
